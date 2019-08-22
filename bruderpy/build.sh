53e9700252f2fdc13d4a595585b527ce3e70bc5c
Gregor Godbersen <git@doamo.de>
2019-08-22 14:14:10 +0200

    Updated Repo URL

--- bruderpy/build.sh
+++ bruderpy/build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo docker run --rm --privileged -v ~/.docker:/root/.docker -v "$(dirname "$(readlink -f "$0")")":/data homeassistant/amd64-builder --all --test
+docker run --rm --privileged -v ~/.docker:/root/.docker -v "$(dirname "$(readlink -f "$0")")":/data homeassistant/amd64-builder --all -t /data
